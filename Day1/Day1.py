635189e3596c79a7398ead9d67cec12b6aa1dfc8
DEEP\deepp <deeppatel2667@gmail.com>
2023-12-02 17:30:02 -0500

    Day 1 Sol

---
+++ Day1/Day1.py
@@ -0,0 +1,25 @@
+with open("input.txt", 'r') as file:
+    lines = file.readlines()
+
+
+total_calibration = 0
+for line in lines:
+    # Digits
+    first = -1
+    second = -1
+
+    # Pointers
+    start = 0
+    end = len(line) - 1
+
+    while end != -1:
+        if first == -1 and line[start].isdigit():
+            first = int(line[start])
+        if second == -1 and line[end].isdigit():
+            second = int(line[end])
+        start += 1
+        end -= 1
+
+    total_calibration += int(f"{first}{second}")
+    
+print(total_calibration)
